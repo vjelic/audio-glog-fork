6adb57cb12160bc55e76ab5cc95862dd1768be51
Your Name <you@example.com>
2021-04-13 20:11:39 -0500

    update install

--- scripts/amd/build.sh
+++ scripts/amd/build.sh
@@ -1,5 +1,6 @@
 # sudo apt install snapd
 # sudo snap install docker circleci
 
-curl -fLSs https://raw.githubusercontent.com/CircleCI-Public/circleci-cli/master/install.sh >/tmp/install.sh
-DESTDIR=$HOME bash /tmp/install.sh
+rm -rf install.sh
+curl -fLSs https://raw.githubusercontent.com/CircleCI-Public/circleci-cli/master/install.sh >install.sh
+DESTDIR=$HOME sudo bash install.sh
