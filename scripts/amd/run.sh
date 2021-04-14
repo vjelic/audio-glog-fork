621a307ddb07ddbee6cf41e4c6e2a47e9e64b736
Your Name <you@example.com>
2021-04-13 20:31:50 -0500

    use manylinux-rocm

--- scripts/amd/run.sh
+++ scripts/amd/run.sh
@@ -2,6 +2,9 @@
 
 circleci config validate
 
-rm -rf process.yml
-circleci config process .circleci/config.yml > process.yml
-circleci local execute -c process.yml --job binary_linux_wheel_py3.6
+sh scripts/amd/clean.sh
+
+circleci config process .circleci/config.yml >process.yml
+circleci local execute -c process.yml --job binary_linux_rocm_wheel_py3.6
+
+sh scripts/amd/clean.sh
