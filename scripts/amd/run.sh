c681a69f2a3b40f5047bc874fb058d61c3b65771
Michael Melesse <micmelesse@gmail.com>
2021-04-16 17:43:13 -0700

    update config yml in

--- scripts/amd/run.sh
+++ scripts/amd/run.sh
@@ -8,8 +8,8 @@ sh scripts/amd/regenerate.sh
 circleci config process .circleci/config.yml >process.yml
 
 # jobs
-# circleci local execute -c process.yml --job binary_rocm_linux_wheel_py3.6
+circleci local execute -c process.yml --job binary_rocm_linux_wheel_py3.6
 # circleci local execute -c process.yml --job nightly_binary_rocm_linux_wheel_py3.6
-circleci local execute -c process.yml --job unittest_linux_gpu_rocm_py3.6
+# circleci local execute -c process.yml --job unittest_linux_gpu_rocm_py3.6
 
-# sh scripts/amd/clean.sh
+sh scripts/amd/clean.sh
